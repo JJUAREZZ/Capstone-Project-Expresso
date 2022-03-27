80d3704cee4e0bff2685567d65bdea6a1c8114cc
JJUAREZZ <joacojuarez1@gmail.com>
2022-03-27 10:50:23 -0300

    api settings

---
+++ api/api.js
@@ -0,0 +1,9 @@
+const express = requiere("express");
+const apiRouter = express.Router();
+const menusRouter = require("./menus.js");
+const employeesRouter = require("./employees.js");
+
+apiRouter.use("/menus", menusRouter);
+apiRouter.use("/employees", employeesRouter);
+
+module.exports = apiRouter;
